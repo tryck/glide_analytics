0e449ecdf3d4a4c7a54ac05becbc86fd0cdf5abb
Rick Karani <ericg.karani@gmail.com>
2026-03-24 01:05:56 +0300

    Update API_BASE for production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import {
   ExternalLink, Pencil, Briefcase, Boxes, MoreVertical
 } from 'lucide-react';
 
-const API_BASE = 'http://localhost:3001/api';
+const API_BASE = '/api';
 
 function Login({ onLogin }) {
   const [username, setUsername] = useState('');
